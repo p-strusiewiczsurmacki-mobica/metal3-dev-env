e2d944b3f10dd72c55a738ca5fccc273d84a17c3
Patryk Strusiewicz-Surmacki <patryk-pawel.strusiewicz-surmacki@external.telekom.de>
2024-02-02 18:26:16 +0100

    Changes
    Signed-off-by: Patryk Strusiewicz-Surmacki <patryk-pawel.strusiewicz-surmacki@external.telekom.de>

--- rerun.sh
+++ rerun.sh
@@ -3,4 +3,4 @@
 make delete_mgmt_cluster
 make host_cleanup
 
-CAPM3PATH=/home/patrykstrusiewicz-surmacki/github/p-strusiewiczsurmacki-mobica/tmp/cluster-api-provider-metal3 CAPM3BRANCH=clusterclass-support CAPM3REPO=https://github.com/p-strusiewiczsurmacki-mobica/cluster-api-provider-metal3.git BUILD_CAPM3_LOCALLY=true make ci_run
+CAPM3PATH=/home/patryk/github/p-strusiewiczsurmacki-mobica/tmp/cluster-api-provider-metal3 CAPM3BRANCH=clusterclass-support CAPM3REPO=https://github.com/p-strusiewiczsurmacki-mobica/cluster-api-provider-metal3.git BUILD_CAPM3_LOCALLY=true make ci_run
